1c93da0a7030b911aedf3c380aa888574031c5dd
Benjamin Chiong <benjamin.chiong@customerservice.nsw.gov.au>
2020-12-17 16:24:30 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import 'nsw-design-system/dist/css/main.css'
+import {Accordion, NSWTemplate} from "nsw-digital-lib";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <NSWTemplate title={'Header'} toggleMenu={true} userName={'Ben'}>
+        </NSWTemplate>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'nsw-design-system/dist/css/main.css'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
